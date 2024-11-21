1bde16a1a042783a06ffb5f8034dbb9eafc08340
LETRUONGIOT98 <168682625+LETRUONGIOT98@users.noreply.github.com>
2024-11-21 19:01:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ app.secret_key = 'your_secret_key'  # Đặt khóa bí mật cho Flask
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'linhkienvang.sell@gmail.com'  # Thay bằng email của bạn
-app.config['MAIL_PASSWORD'] = 'pbiwvptfncfwfwji'  # Mật khẩu ứng dụng
+app.config['MAIL_USERNAME'] = '*************************'  # Thay bằng email của bạn
+app.config['MAIL_PASSWORD'] = '*************************'  # Mật khẩu ứng dụng
 
 mail = Mail(app)
 
